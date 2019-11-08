8691fea46782720fbca4bac86c608e42dbf33af1
Prach Butpa <prachzas_sut@hotmail.com>
2019-11-09 00:40:30 +0700

    Deploy v.2

--- timer/src/App.js
+++ timer/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './bootstrap.css';
 import './App.css';
 import Timer from './Timer'
-import {HashRouter as Router} from 'react-router-dom';
 function App() {
   return (
     <div>
