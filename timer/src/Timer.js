97613fb8df19538ef733f1384ec06095e77d9b38
Prach Butpa <prachzas_sut@hotmail.com>
2019-11-11 14:37:20 +0700

    Update code v.2

--- timer/src/Timer.js
+++ timer/src/Timer.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react'
+import logoScb from './scb-logo.png'
 export default class Timer extends Component {
     constructor(props){
         super(props)
         this.state = {
-            minutes: Number,
-            seconds: Number,
-            input: undefined
+            minutes: 0,
+            seconds: "",
+            input: Number,
+            isRunning: undefined
         }
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
-    
     }
+    
     handleSubmit(){
         this.setState({
             minutes: this.state.input,
-            seconds: 0
+            seconds: 0,
+            isRunning: true
         })
+        this.countdown()
     }
     handleChange(event){
         this.setState({
@@ -23,19 +27,31 @@ export default class Timer extends Component {
         })
         
     }
-    componentDidMount() {
+    countdown(){
         this.myInterval = setInterval(() => {
             const { seconds, minutes } = this.state
 
+            if(minutes === 0 && seconds === 0){
+                clearTimeout(this.myInterval)
+                console.log("clearInterval แล้วนะ")
+                this.setState({
+                    isRunning: false
+                })
+                console.log(this.state.isRunning)
+            }
             if (seconds > 0) {
+                console.log("second > 0")
                 this.setState(({ seconds }) => ({
                     seconds: seconds - 1
                 }))
             }
             if (seconds === 0) {
+                console.log("second == 0")
                 if (minutes === 0) {
+                    console.log("minutes == 0")
                     clearInterval(this.myInterval)
                 } else {
+                    console.log("minutes != 0")
                     this.setState(({ minutes }) => ({
                         minutes: minutes - 1,
                         seconds: 59
@@ -43,10 +59,6 @@ export default class Timer extends Component {
                 }
             } 
         }, 1000)
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.myInterval)
     }
     
     render() {
@@ -60,13 +72,18 @@ export default class Timer extends Component {
                             <div id="cover-caption">
                                 <div className="row text-white">
                                     <div className="col-sm-6 offset-sm-3 text-center">
-                                        <h1 className="display-4">Timer App</h1>
+                                    <div class="corner-ribbon top-right sticky blue">Digital Academy</div>
+                                    <img className="logo" src= {logoScb} alt = "scb logo"  />
+                                        {/* <h1 className="display-4">Timer App</h1> */}
+                                        <br></br>
+                                        <br></br>
+                                        <br></br>
                                         <div className="info-form">
                                             <div className="form-group">
                                                 <label className="sr-only">Name</label>
                                                 <input className="form-control text-center" placeholder="Minute..." type="number" onChange={this.handleChange} />
                                             </div>
-                                            <button className="btn btn-success" disabled={!this.state.input} 
+                                            <button className="btn btn-success" disabled={!this.state.input || this.state.isRunning===true} 
                                                 style={{fontSize: "13px"}} type="submit" 
                                                 onClick={this.handleSubmit}>
                                                 START
@@ -76,12 +93,12 @@ export default class Timer extends Component {
                                 </div>
                                 <div>
                                     <hr />
-                                    <h1 className="text-white">
+                                    <div className="text-white">
                                     { minutes === 0 && seconds === 0
                                         ? <h1>Time Out !</h1>
                                         : <h1>Minute : {minutes} Second : {seconds < 10 ? `0${seconds}` : seconds}</h1>
                                     }
-                                    </h1>
+                                    </div>
                                 </div>
                             </div>
                         </section>
